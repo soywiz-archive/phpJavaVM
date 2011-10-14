15b28f15e5c664df780e79487df316ae43f74579
soywiz <soywiz@gmail.com>
2011-10-15 01:41:01 +0200

    - Removed old files

--- _old/dynaload/test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-bin2c php_dynacall_init.php php_dynacall_init.php.c dynacall_init_php > NUL
-\dev\tcc\tiny_impdef.exe php5ts.dll > NUL
-\dev\tcc\tcc.exe -shared php_dynacall.c php_dynacall_init.php.c php5ts.def -lkernel32 -o ext\php_dynacall.dll && php test.php
--- _old/dynaload/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-MessageBox(0, 'Hola', 'Prueba', 0);
-SDL_Init(0x20);
-SDL_SetVideoMode(640, 480, 0, 0);
